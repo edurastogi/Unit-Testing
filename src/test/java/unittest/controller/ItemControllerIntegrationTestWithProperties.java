0bf2090c7be7f0ea8e1700e3a161be7e828dc5d5
aditya rastogi <edurastogi@gmail.com>
2018-09-25 07:24:50 -0400

    Integration Testing

--- src/test/java/unittest/controller/ItemControllerIntegrationTestWithProperties.java
+++ src/test/java/unittest/controller/ItemControllerIntegrationTestWithProperties.java
@@ -7,11 +7,13 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ItemControllerIT {
+@TestPropertySource(locations={"classpath:test-configuration.properties"})
+public class ItemControllerIntegrationTestWithProperties {
 
     @Autowired
     private TestRestTemplate restTemplate;
